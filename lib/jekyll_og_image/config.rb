166544fd4f953adddbf58a0998eeba95ec0ab6a6
Igor Alexandrov <igor.alexandrov@gmail.com>
2024-02-05 10:14:34 +0400

    Added GitHub workflow to run RSpec

--- lib/jekyll_og_image/config.rb
+++ lib/jekyll_og_image/config.rb
@@ -7,12 +7,12 @@ class JekyllOgImage::Config < Anyway::Config
   attr_config force: false
   attr_config :domain
   attr_config border_bottom: {
-    size: 20,
+    width: 20,
     fill: [ "#211F1F", "#F4CBB2", "#AD5C51", "#9CDAF1", "#7DBBE6" ]
   }
 
   coerce_types border_bottom: {
-    size: { type: :integer },
+    width: { type: :integer },
     fill: { type: :string, array: true }
   }
 end
